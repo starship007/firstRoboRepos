6e6bb5e79f5b26a7d9babeb76bf08533563b8cac
starship007 <thisiscodyr@gmail.com>
2020-01-19 22:52:08 -0600

    hi

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
+//this is edit two - will Git WORK?
 //please ignore this project, there wasn't anything - its just to test GIT through this
 //please ignore this project, there wasn't anything - its just to test GIT through this
 //please ignore this project, there wasn't anything - its just to test GIT through this
@@ -16,7 +17,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
+import java.util.Random;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
